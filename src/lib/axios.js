a1af52db566cb7c2012adb2a088dfd7a9291e94d
RamFarid <workprojects22@gmail.com>
2024-01-28 21:19:06 +0200

    add url of server

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const server = axios.create({
-  baseURL: import.meta.env.DEV ? 'http://localhost:5000' : 'else',
+  baseURL: import.meta.env.DEV
+    ? 'http://localhost:5000'
+    : 'https://ahiah2ahiah-server.vercel.app',
 })
 
 export default server
